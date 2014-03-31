43daa0ad31e08486d6646ae971cb657989915086
ariell <a.friedman@acfr.usyd.edu.au>
2014-03-31 16:26:47 +1100

    updates to wsgi

--- catamiPortal/wsgi.py
+++ catamiPortal/wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 import sys
 
-sys.path.insert(0, '/home/catami/catamiportal/catami')
+sys.path.insert(0, '/home/auv/git/squidle')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "catamiPortal.settings")
 
