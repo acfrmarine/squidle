1b869b00acd3cac0f030940b44ac860ea872e377
Navid Nourani-Vatani <navid@nourani.dk>
2014-04-22 12:28:11 +1000

    changed the btn class

--- webinterface/static/assets/catami-api/js/map-ui.js
+++ webinterface/static/assets/catami-api/js/map-ui.js
@@ -1198,9 +1198,9 @@ function BaseMap(geoserverUrl, deploymentExtentUrl, collectionExtentUrl, globals
 	 */
     this.addBBoxSelect = function ($container, $infocontainer,layername) {
 		console.log("Function addBBoxSelect");
-        var $bboxdraw = $('<button type="button" id="bboxdraw" class="btn-group btn-group-sm" title="Draw a bounding box around the images you would like to add to your selection."><i class="icon-crop"></i> Create</button>'),
-			$bboxedit = $('<button type="button" id="bboxedit" class="btn-group btn-group-sm" title="Edit a bounding box by selecting it."><i class="icon-edit"></i> Edit</button>'),
-			$bboxdel  = $('<button type="button" id="bboxdel"  class="btn-group btn-group-sm" title="Delete a bounding box by selecting it."><i class="icon-remove-sign"></i> Delete</button>');
+        var $bboxdraw = $('<button type="button" id="bboxdraw" class="btn btn-group btn-group-sm" title="Draw a bounding box around the images you would like to add to your selection."><i class="icon-crop"></i> Create</button>'),
+			$bboxedit = $('<button type="button" id="bboxedit" class="btn btn-group btn-group-sm" title="Edit a bounding box by selecting it."><i class="icon-edit"></i> Edit</button>'),
+			$bboxdel  = $('<button type="button" id="bboxdel"  class="btn btn-group btn-group-sm" title="Delete a bounding box by selecting it."><i class="icon-remove-sign"></i> Delete</button>');
 		// Setup button action callbacks
         $bboxdraw.click(function (){
             toggleBBoxDraw();
