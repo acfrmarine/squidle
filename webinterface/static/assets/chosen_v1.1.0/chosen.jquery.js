5478312d07e41f95681710c8f7e2efe53338891b
Navid Nourani-Vatani <navid@nourani.dk>
2014-05-19 17:28:14 +1000

    set the li id

--- webinterface/static/assets/chosen_v1.1.0/chosen.jquery.js
+++ webinterface/static/assets/chosen_v1.1.0/chosen.jquery.js
@@ -260,6 +260,7 @@
             option_el.className = classes.join(" ");
             option_el.style.cssText = option.style;
             option_el.setAttribute("data-option-array-index", option.array_index);
+            option_el.setAttribute("id", option.value);
             console.log(option);
             //zoom = '&nbsp;<a href="javascript: void(0);"><img src="https://cdn1.iconfinder.com/data/icons/large-black-icons/512/Zoom_in_magnifying_glass.png" height="10px" /></a>&nbsp' + option.search_text;
             zoom = '&nbsp;';
