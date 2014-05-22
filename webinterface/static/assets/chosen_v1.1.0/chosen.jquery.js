2bb0565f8ca17c3388cb009240e8fbd3fa396961
Navid Nourani-Vatani <navid@nourani.dk>
2014-05-23 07:33:30 +1000

    appending shortlist class tag

--- webinterface/static/assets/chosen_v1.1.0/chosen.jquery.js
+++ webinterface/static/assets/chosen_v1.1.0/chosen.jquery.js
@@ -390,6 +390,7 @@
                     }
                 }
                 else if(option.disabled){
+                    console.log(option);
                     shortList = true;
                 }
             }
