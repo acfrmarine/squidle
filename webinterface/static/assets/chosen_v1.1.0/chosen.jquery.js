ee6aa011528e8a10ea38ff0f8894291c89d2d4ea
Navid Nourani-Vatani <navid@nourani.dk>
2014-06-11 14:54:52 +1000

    debugging trigger

--- webinterface/static/assets/chosen_v1.1.0/chosen.jquery.js
+++ webinterface/static/assets/chosen_v1.1.0/chosen.jquery.js
@@ -404,18 +404,12 @@
                 this.update_results_content(this.results_option_build());
                 
 				var _this = this;
-				var _ffjq = this.form_field_jq;
 				var $showing = $('<span class="badge badge-sm" style="margin: 5px; z=1100;">Showing ' + results_nogroup +'/'+ total_nogroup + '</span>');
 				var $showView = $('<span class="badge badge-sm" style="margin: 5px; z=1100;">Show in view&nbsp<a href="javascript: void(0);"><i class="icon-remove-sign chosen-viewport"></i></a></span>');
 				var $showAll = $('<span class="badge badge-sm" style="margin: 5px; z=1100;">Show all&nbsp<a href="javascript: void(0);"><i class="icon-remove-sign chosen-everything"></i></a></span>');
 				$showView.find('a').click( function() {
 					console.log('click:show visible');
-					console.log(_this.form_field_jq);
-					_ffjq.trigger("chosen:new_results", {
-						chosen: _this,
-						param: 'show visible'
-					});
-					return;
+					_this.trigger_visible();
 				});
 				$showAll.find('a').click( function() {
 					console.log('click:show all');
@@ -1030,6 +1024,12 @@
             });
             this.results_update_field();
         }
+		
+		Chosen.prototype.trigger_visible = function() {
+			this.form_field_jq.trigger( "chosen:show_visible", {
+				chosen: this
+			});
+		}
 
         Chosen.prototype.search_results_mouseup = function (evt) {
             var target;
